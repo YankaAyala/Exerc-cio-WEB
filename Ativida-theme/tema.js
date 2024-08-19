ba8d8a7a9e8b503cf4c743858e79594d696950b3
Yanka Ayala <119422105+YankaAyala@users.noreply.github.com>
2024-08-18 21:16:04 -0300

    Add files via upload

---
+++ Ativida-theme/tema.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+      extend: {
+        borderRadius: {
+            'xl': '1.5rem',
+            colors: {
+             primary: '#1E40AF', 
+            secondary: '#F97316', 
+            accent: '#10B981', 
+        },
+      },
+    },
+    plugins: [],
+  }
+}
